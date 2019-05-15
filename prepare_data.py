5eef8430030292817cf1e82a36eb423fb5fa19dd
Amy Jordan <amy_jordan@live.co.uk>
2019-05-15 16:38:22 +0100

    First model training and set up Sets up train.py to train our model Updates prepare_data.py to confirm data should be reshaped=True§

--- prepare_data.py
+++ prepare_data.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-def load(dir, files):
+def load(dir, files, reshaped):
     "returning list of numpy arrays"
 
     data = []
     for file in files:
         f = np.load(dir + file)
-
-        for i in range(len(f)):
-            x = np.reshape(f[i], (28, 28))
-            x = np.expand_dims(x, axis=0)
-            x = np.reshape(f[i], (28, 28, 1))
-            data.append(x)
-
+        if reshaped:
+            new_f = []
+            for i in range(len(f)):
+                x = np.reshape(f[i], (28, 28))
+                x = np.expand_dims(x, axis=0)
+                x = np.reshape(f[i], (28, 28, 1))
+                new_f.append(x)
+            f = new_f
+        data.append(f)
     return data
 
 def normalize(data):
@@ -30,6 +32,9 @@ def set_limit(arrays, n):
                 break
             limited_list.append(item)
             i += 1
+    # print(n)
+    # print(len(arrays))
+    # print(len(limited_list))
     return limited_list
 
 def generate_labels(classes, samples):
@@ -41,5 +46,5 @@ def generate_labels(classes, samples):
 
 # data = load('data/', ['circles.npy'])
 # normalize(data)
-# print(set_limit([[0,1,2,3,4,5,6,7,8,9]], 2))
+# print(set_limit([[0,1,2,3,4,5,6,7,8,9], [9, 8, 7, 6, 5, 4, 3, 2, 1]], 2))
 # print(generate_labels(5, 4))
