9f3ef598a7efb116b4cc5b595f413822e5a04f3a
James Palmer <jamesdcpalmer1@gmail.com>
2019-05-15 14:06:53 +0100

    TRAIN: set up train

---
+++ train.py
@@ -0,0 +1,6 @@
+from prepare_data import *
+from sklearn.model_selection import train_test_split as tts
+from kerals.utils import np_utils
+from nets.MLP import mlp
+from nets.conc import conv
+from random import randint
