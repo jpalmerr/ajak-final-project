870e795428bce86138f40684892850d5d9a8e564
Amy Jordan <amy_jordan@live.co.uk>
2019-05-16 10:19:47 +0100

    Adds test for generate labels to return an empty array if numbers provided are 0

--- tests/units/prepare_data_test.py
+++ tests/units/prepare_data_test.py
@@ -6,4 +6,7 @@ from prepare_data import *
 def test_generate_labels_returns_array():
     assert generate_labels(2, 2) == [0, 0, 1, 1]
 
+def test_generate_labels_returns_empty_array():
+    assert generate_labels(0, 0) == []
+
 # print(set_limit([[0,1,2,3,4,5,6,7,8,9], [9, 8, 7, 6, 5, 4, 3, 2, 1]], 2))
