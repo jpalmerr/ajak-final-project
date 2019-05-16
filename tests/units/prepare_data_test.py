d46a9837b71eed441228e1055fdf0ad7044cee86
Amy Jordan <amy_jordan@live.co.uk>
2019-05-16 10:21:48 +0100

    Adds test for #set_limit to return array with set number of items from each array

--- tests/units/prepare_data_test.py
+++ tests/units/prepare_data_test.py
@@ -3,10 +3,11 @@ import sys
 sys.path.append('../ajak-final-project/lib')
 from prepare_data import *
 
+def test_set_limit_generates_single_limited_list():
+    assert set_limit([[0,1,2,3], [9,8,7,6]], 2) == [0,1,9,8]
+
 def test_generate_labels_returns_array():
     assert generate_labels(2, 2) == [0, 0, 1, 1]
 
 def test_generate_labels_returns_empty_array():
     assert generate_labels(0, 0) == []
-
-# print(set_limit([[0,1,2,3,4,5,6,7,8,9], [9, 8, 7, 6, 5, 4, 3, 2, 1]], 2))
