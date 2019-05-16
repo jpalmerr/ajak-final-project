9377e2beccd68b6ae2ced8132e164e67563f83a6
Amy Jordan <amy_jordan@live.co.uk>
2019-05-16 10:23:30 +0100

    Adds test for #set_limit for empty arrays

--- tests/units/prepare_data_test.py
+++ tests/units/prepare_data_test.py
@@ -6,6 +6,9 @@ from prepare_data import *
 def test_set_limit_generates_single_limited_list():
     assert set_limit([[0,1,2,3], [9,8,7,6]], 2) == [0,1,9,8]
 
+def test_set_limit_generates_empty_array():
+    assert set_limit([[],[]], 2) == []
+
 def test_generate_labels_returns_array():
     assert generate_labels(2, 2) == [0, 0, 1, 1]
 
