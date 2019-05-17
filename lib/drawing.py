eea6c5024c0f7f9e1dfce0379db6ef1535e4097a
Amy Jordan <amy_jordan@live.co.uk>
2019-05-17 12:33:12 +0100

    Amends Drawing #reshape to return greyscale image Need to continue work on prediction

--- lib/drawing.py
+++ lib/drawing.py
@@ -7,7 +7,9 @@ class Drawing():
         self.image = image
 
     def reshape(self):
-        img = (self.image).resize((28,28))
+        img = (self.image).resize((28,28)).convert('L')
         np_img = np.array(img)
+        np_img = np.expand_dims(np_img, axis=0)
+        np_img = np.reshape(np_img, (28,28,1))
         np_img = normalize(np_img)
         return np_img
--- lib/prediction.py
+++ lib/prediction.py
@@ -1,4 +1,19 @@
+from keras.models import load_model
+import sys
+import numpy as np
+sys.path.append('./helper_modules')
+from constants import SHAPES
 
 class Prediction():
     def __init__(self, image):
         self.image = image
+
+    def predict(self):
+        model = load_model("./models/first_model_training.h5")
+        print('we have loaded the model')
+        print(np.array(self.image))
+        pred = SHAPES[np.argmax(model.predict(np.array(self.image)))]
+        print('this is our prediction')
+        print(pred)
+        print('this is the end of the prediction')
+        return 1
