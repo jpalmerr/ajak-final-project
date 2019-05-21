20972fd5ab3acbdb59af7d0da0e1859bb697682e
Amy Jordan <amy_jordan@live.co.uk>
2019-05-21 10:17:18 +0100

    Addresses minor pylint issues
    Co-authored-by: Amy Jordan <amy_jordan@live.co.uk>
    Co-authored-by: Kim Diep <kim.diep.techno@gmail.com>

--- lib/prediction.py
+++ lib/prediction.py
@@ -6,7 +6,7 @@ from constants import ITEMS
 
 class Prediction():
     def __init__(self, drawing):
-        self.drawing= drawing
+        self.drawing = drawing
 
     def predict(self):
         model = load_model("./models/first_model_training.h5")
