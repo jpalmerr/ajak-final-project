80fd50802ab34f69bec8599d8befd660bbf44591
Amy Jordan <amy_jordan@live.co.uk>
2019-05-22 11:42:50 +0100

    Adds tests for predict and clear buttons
    Co-authored-by: Amy Jordan <amy_jordan@live.co.uk>
    Co-authored-by: Kim Diep <kim.diep.techno@gmail.com>

--- tests/features/index_test.py
+++ tests/features/index_test.py
@@ -15,12 +15,20 @@ def test_home_page_header():
 
     browser.quit()
 
-# def test_click_predict():
-#
-#     browser = Browser('flask', app=app)
-#
-#     browser.visit('http://localhost:5000/')
-#     # browser.execute_script("document.getElementById('predictButton').click()")
-#     browser.find_by_id('predictButton').click()
-#     assert browser.is_text_present("Predicted Output")
-#     browser.quit()
+def test_views_predict_button():
+
+    browser = Browser('flask', app=app)
+
+    browser.visit('http://127.0.0.1:5000/')
+    assert browser.find_by_id("predictButton")
+
+    browser.quit()
+
+def test_views_clear_button():
+
+    browser = Browser('flask', app=app)
+
+    browser.visit('http://127.0.0.1:5000/')
+    assert browser.find_by_id("clear-canvas")
+
+    browser.quit()
