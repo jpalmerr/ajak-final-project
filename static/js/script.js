848f2ba147296a165ade44a2e765acd615896d09
alexanderchen6 <alexbaggio315@hotmail.com>
2019-05-22 10:53:37 +0100

    user can see how many guessed correct and total amount of guesses, as well as percentage

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,63 @@ $("#predictButton").click(function(event){
     }
   });
 });
+
+function correctCounter() {
+
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("totalAttempts").innerHTML = localStorage.clickcount;
+    document.getElementById("correctAttempts").innerHTML = localStorage.clickcount;
+    percentageTracker()
+}
+
+function incorrectCounter() {
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount)+1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+  document.getElementById("totalAttempts").innerHTML = localStorage.clickcount;
+  percentageTracker()
+
+}
+
+function percentageTracker(){
+  var totalAttempts= parseInt(document.getElementById('totalAttempts').innerHTML)
+  var correctAttempts = parseInt(document.getElementById('correctAttempts').innerHTML)
+  var percentage = (correctAttempts / totalAttempts) * 100 
+  document.getElementById("percentageCorrect").innerHTML = Math.round(percentage)
+
+}
+
+
+
+
+
+
+
+
+// $("#correctPrediction").click(function(event){
+//   event.preventDefault()
+
+//   var n = localStorage.getItem('totalAttempts')
+
+// // create localstorage
+
+//   console.log(n)
+
+//   if (n === null) {
+
+//     n = 0;
+//   }
+//   n++
+
+//   localStorage.setItem('totalAttempts', n);
+
+//   document.getElementById('totalAttempts').innerHTML = n
+
+// increase count
+// })
